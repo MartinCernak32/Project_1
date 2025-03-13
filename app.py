466654ee4f12cf238b755c1e939477f421a05b17
Martin Cernak <martin.cernak32@gmail.com>
2025-03-13 15:43:31 +0100

    A few additions

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
-print("hello worl")
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":basketball:", layout="wide")
+
+# ----- LOADING ASSETS ----- #
+
+# ----- LOAD ASSETS ----- #
+
+
+
+# ----- Header Section ----- #
+with st.container():
+    st.subheader("Hi, I am Martin :wave:")
+    st.title("An aspiring software developer")
+    st.write("I like learning about python!!")
+    
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            Hi my name is Martin, I come from Serbia and i really enjoy programming.
+            
+            """              
+        )
+    with right_column:
+        
+    
+    
+    
+    
+    
+    
+    
