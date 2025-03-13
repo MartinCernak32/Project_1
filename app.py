9759780fb664c5eaba4b1cf26e6dcaa6ccaf4a2b
Martin Cernak <martin.cernak32@gmail.com>
2025-03-13 16:51:57 +0100

    Added a contact form and style.css

--- app.py
+++ app.py
@@ -13,9 +13,13 @@ st.set_page_config(page_title="My Webpage", page_icon=":basketball:", layout="wi
 
 # ----- Header Section ----- #
 with st.container():
-    st.subheader("Hi, I am Martin :wave:")
-    st.title("An aspiring software developer")
-    st.write("I like learning about python!!")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.subheader("Hi, I am Martin :wave:")
+        st.title("An aspiring software developer")
+        st.write("I like learning about python!!")
+    with right_column:
+        st.image("Capture.PNG", caption="Me")
     
 with st.container():
     st.write("---")
@@ -30,6 +34,29 @@ with st.container():
             """              
         )
     with right_column:
+        st.write("##")
+        
+    # --- CONTACT --- #
+    with st.container():
+        st.write("")
+        st.header("Get In Touch With Me")
+        contact_form = """
+        <input type="hidden" name="_captcha" value="false">
+        <form action="https://formsubmit.co/crki73@gmail.com" method="POST">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your e-mail" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+        </form>
+        """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
+        
+        
+        
         
     
     
